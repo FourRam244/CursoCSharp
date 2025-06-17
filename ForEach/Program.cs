c91849af30393d9c4c585b8c8586651e20bc415d
Cesar Ramirez <cesaram244@gmail.com>
2025-06-17 13:11:05 -0600

    Actualizacion curso

---
+++ ForEach/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEach
+//foreach es una estructura de control que se utiliza
+//para recorrer colecciones como arreglos, listas, diccionarios
+//y otros tipos que implementan IEnumerable.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var te ahorra escribir codigo ya que se determina con lo escribas a la derecha
+            //List<int> numbers = new List<int>()  forma normal
+            var numbers = new List<int>()  //forma reducida
+            {
+                23,3,5,10,22,12
+            };
+            foreach (var number in numbers)  //recorido mas natural que while
+            {
+                Console.WriteLine(number);
+            }
+
+            var students = new List<People>()
+            {
+                new People(){ Name = "Cesar", Country="Mexico"},
+                new People(){ Name = "Roberto", Country="Argentina"},
+                new People(){ Name = "Anna", Country="Colombia"},
+            };
+
+            Show(students);
+
+            students.RemoveAt(0); //Remover algo especificando la posicion
+
+            Show(students);
+        }
+        static void Show(List<People> students)
+        {
+            Console.WriteLine("-- Personas --");
+            foreach (var people in students)
+            {
+                Console.WriteLine($"Nombre {people.Name}, Pais {people.Country}");
+            }
+        }
+    }
+
+    class People
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ MetodosComunesListas/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetodosComunesListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            {
+                4,3,5,19
+            };
+
+            Show(numbers);
+            numbers.Insert(1,6); //Insert: Insertar algo en una posicion especifica
+            Show(numbers);
+
+            if (numbers.Contains(33))  //Contains: Regresa true o false, para validar datos de una lista
+                Console.WriteLine("Existe");
+            else
+            {
+                Console.WriteLine("No existe");
+            }
+
+            //IndexOf //regresa la posicion de el numero dado, -1 si el elemnto no existe
+            int pos = numbers.IndexOf(19);
+            Console.WriteLine(pos);
+
+            //Sort: para ordenar una lista de elementos
+            numbers.Sort();
+            Show(numbers);
+
+            //Add Range agregar otra lista al final de la lista
+            numbers.AddRange(new List<int>()
+            {
+                300,200,400
+            });
+
+            Show(numbers);
+        }
+
+        public static void Show(List<int> numbers)
+        {
+            Console.WriteLine("-- Numeros --");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
