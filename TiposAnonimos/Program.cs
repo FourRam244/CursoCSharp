29c4537d1de68813b7157084c68aab0626c775fc
Cesar Ramirez <cesaram244@gmail.com>
2025-06-17 15:08:20 -0600

    Actualizacion

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TiposAnonimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esto es solo un objeto aninimo que solo es de lectura no se pueden modificar sus valores
+            var cesar = new
+            {
+                Name = "Cesar",
+                Country = "México"
+            };
+            Console.WriteLine($"{cesar.Name} {cesar.Country}");
+
+            var beers = new[]
+            {
+                new {Name = "Red", brand= "Delirium"},
+                new {Name = "Corona", brand= "Modelo"},
+            };
+
+            foreach ( var b in beers ) 
+            {
+                Console.WriteLine($"cerveza{b.Name} {b.brand}");
+            }
+    }
+    }
+}
---
+++ Tuplas/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Tuplas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esto es una tupla
+            (int id, string name) product = (1, "cerveza stout");
+
+            Console.WriteLine($"{product.id} {product.name}");
+            //La tupla si es editable
+            product.name = "cerverza modelo";
+            Console.WriteLine($"{product.id} {product.name}");
+
+            //Segunda forma de escribir una tupla
+            var person = (1, "Cesar");
+            Console.WriteLine($"persona {person.Item1} {person.Item2}");
+
+            //Areglo de tuplas
+            var people = new[]
+            {
+                (1, "Cesar"),
+                (2, "Pedro"),
+                (3, "Juan"),
+            };
+
+            foreach (var p in people)
+            {
+                Console.WriteLine($"{p.Item1} {p.Item2}");
+            }
+            //Conexion de tuplas
+            (int id, string name)[] people2 = new[]
+            {
+                (1, "Cesar"),
+                (2, "Pedro"),
+                (3, "Juan"),
+            };
+
+            foreach (var p in people2)
+            {
+                Console.WriteLine($"{p.id} {p.name}");
+            }
+
+
+            var cityInfo = getLocationCDMX();
+            Console.WriteLine($"lat: {cityInfo.lat} long:{cityInfo.lng} nombre:{cityInfo.name}");
+
+            //Se usan _ para ignorar algo, obteniendo un valor especifico de la tupla
+            var (_, lng, _) = getLocationCDMX();
+
+            Console.WriteLine(lng);
+
+        }
+        //Metodo con tuplas
+        public static (float lat, float lng, string name) getLocationCDMX()
+        {
+            float lat = 19.12121f;
+            float lng = -99.1921f;
+            string name = "CDMX";
+
+
+            return (lat, lng, name);
+        }
+    }
+}
